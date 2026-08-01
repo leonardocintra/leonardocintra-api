4372fa1fbd284f5369baefe4bc848017c0e6d50c
Leonardo Cintra <leonardo.ncintra@outlook.com>
2026-08-01 16:43:10 -0300

    chore: log de mensagem investigativa

--- src/whatsapp/mensagem/mensagem.controller.ts
+++ src/whatsapp/mensagem/mensagem.controller.ts
@@ -9,7 +9,14 @@ export class MensagemController {
   constructor(private readonly mensagemService: MensagemService) { }
 
   @Post('receber')
-  receberMensagem(@Body() mensagem: ReceberMensagemDto) {
-    return this.mensagemService.receberMensagem(mensagem);
+  receberMensagem(@Body() body: unknown) {
+    try {
+      console.log('Recebendo mensagem whatsapp:', body);
+      const mensagem = body as ReceberMensagemDto;
+      return this.mensagemService.receberMensagem(mensagem);
+    } catch (error) {
+      console.error('Erro ao receber mensagem whatsapp:', error);
+      throw error;
+    }
   }
 }
