e7c83de8d79823c0d1fb6443f1b51ed760aab682
Leonardo Cintra <leonardo.ncintra@outlook.com>
2026-08-04 20:42:18 -0300

    fix: afiliado module

--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AwsModule } from 'src/aws/aws.module';
 import { WhatsappModule } from 'src/whatsapp/whatsapp.module';
 import { WhatsAppWorkerService } from './whatsapp-worker.service';
+import { AfiliadosModule } from 'src/afiliados/afiliados.module';
 
 @Module({
-  imports: [AwsModule, WhatsappModule],
+  imports: [AwsModule, WhatsappModule, AfiliadosModule],
   providers: [WhatsAppWorkerService],
 })
-export class WorkerModule {}
+export class WorkerModule { }
