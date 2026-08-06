2af550ebf8831ea455284c8dafcf52ac36db2aa5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2026-08-06 07:48:09 -0300

    chore: teste mensagem

--- src/afiliados/afiliados.service.ts
+++ src/afiliados/afiliados.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AfiliadosService {
+
   converterMensagem(mensagem: string): string {
-    const mensagemConvertida = mensagem.replace('https://pechin.co/whatsapp', 'https://www.aviseiprecobom.com.br/');
+    const mensagemConvertida = "Teste Leonardo - Ainda testando";
     return mensagemConvertida;
   }
 }
