4e8a80feb8fa783ddf15a41724eb3a6d9221d7e8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2026-09-08 13:25:32 -0300

    feat: add notifications url

--- src/afiliados/integracao/integracao.controller.ts
+++ src/afiliados/integracao/integracao.controller.ts
@@ -8,8 +8,11 @@ export class IntegracaoController {
   @Get('mercadolivre/callback')
   async mercadolivreCallback(@Query('code') code?: string) {
     // Lógica para lidar com o callback do Mercado Livre
-    // parametro code= gerar log
-
     return { message: `Callback do Mercado Livre recebido com sucesso! Code: ${code}` };
+  }
+
+  @Get('mercadolivre/notifications')
+  async mercadolivreNotifications() {
+    return { message: `Notificação do Mercado Livre recebido com sucesso!` };
   }
 }
